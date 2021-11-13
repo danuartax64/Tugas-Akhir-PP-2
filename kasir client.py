5d541aec5fb77d7e2aa51bcff823e8373ed58232
Muhammad Arya Danuarta <72311684+danuartax64@users.noreply.github.com>
2021-11-13 21:03:06 +0700

    Update kasir client.py

--- kasir client.py
+++ kasir client.py
@@ -6,22 +6,48 @@ from threading import Thread
 s = socket.socket()
 
 def menu():
-    print("======================Client Kasir====================")         #fungsi untuk pilihan menu
-    print("===Menu===============================================")
-    print("1. Input Data Barang Keluar")
-    print("2. Delete Data Barang Keluar")
-    print("3. Keluar...")
+    while True:
+        print("======================Client Kasir====================")         #fungsi untuk pilihan menu
+        print("===Menu===============================================")
+        print("1. Input Data Barang Keluar")
+        print("2. Delete Data Barang Keluar")
+        print("3. Keluar...")
+    
+        masukan = int(input("Pilihan: "))                                      #asking pilihan user
+
+        if masukan == 1:
+            barang = input("Nama Barang keluar: ")
+            harga = int(input("Harga Barang: "))
+            kuantitas = int(input("Jumlah: "))
+            if kuantitas > 1:
+                total = harga * kuantitas
+            else:
+                pass
+            hargastr = str(harga)
+            kuantistr = str(kuantitas)
+            totalstr = str(total)
+            msg = barang + hargastr + kuantistr + totalstr
+            s.send((client + ' : ' + msg).encode())                            #kirim data barang ke server
 
 def connecting():
     try:
         host = input('Masukkan IP Server: ')                                #fungsi untuk connecting socket ke sisi server
-        port = input ('Masukkan Port Server: ')
+        port = int(input('Masukkan Port Server: '))
         s.connect((host,port))
     
     except:
         print("-> Error tidak dapat menyambungkan, mohon dicek kembali host dan port tujuan atau sisi server belum dibuka")
         time.sleep(1)
         return "error"
-
 if __name__ == '__main__':
+
     client = input('Masuk sebagai: ')
+    try:
+        connecting()
+        if connecting == 'error':
+            print("Ada yang salah dengan host / port server")
+            print("Closing App...")
+        else:
+            menu()
+    except:
+        pass
